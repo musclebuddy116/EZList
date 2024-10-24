7435092e0bf0ec684dd73a12ceb6e62a8d211393
ReidTX <reid.boulet@gmail.com>
2024-10-24 00:12:34 -0500

    Connection to AWS working

--- app/src/main/java/com/example/ezlist/MainActivity.java
+++ app/src/main/java/com/example/ezlist/MainActivity.java
@@ -1,25 +1,58 @@
 package com.example.ezlist;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String DATABASE_NAME = "grocery_store_data";
+    private static final String URL = "jdbc:mysql://18.117.171.203:3306/"+DATABASE_NAME;
+    private static final String USER = "android";
+    private static final String PASSWORD = "android";
+    public static final String TABLE_NAME = "grocery_store";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        //Push working
+        try {
+            utilFun();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void utilFun() throws SQLException {
+        new Thread(() -> {
+            StringBuilder records = new StringBuilder();
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                Statement statement = connection.createStatement();
+
+                ResultSet rs = statement.executeQuery("SELECT * FROM " + TABLE_NAME);
+                while (rs.next()) {
+                    String columnValue = rs.getString("name");
+                    Log.d("Database Result", columnValue);
+                }
+
+                connection.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            //Push working
+        }).start();
     }
 }
