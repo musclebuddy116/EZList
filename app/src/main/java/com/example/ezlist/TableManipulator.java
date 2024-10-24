685d471c41dd67adef65589e059f4286a60100b1
nschool3424 <nbsinclair7@gmail.com>
2024-10-24 12:51:50 -0500

    Added TableManipulator class, addItemToTable() function (still not generalized)

--- app/src/main/java/com/example/ezlist/TableManipulator.java
+++ app/src/main/java/com/example/ezlist/TableManipulator.java
@@ -1,39 +1,25 @@
 package com.example.ezlist;
 
-import android.os.Bundle;
-import android.util.Log;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class MainActivity extends AppCompatActivity {
-
-    private static final String DATABASE_NAME = "grocery_store_data";
-    private static final String URL = "jdbc:mysql://18.117.171.203:3306/"+DATABASE_NAME;
-    private static final String USER = "android";
-    private static final String PASSWORD = "android";
-    public static final String TABLE_NAME = "grocery_store";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        try {
-            utilFun();
-            makeListDB();
-            TableManipulator lm = new TableManipulator(DATABASE_NAME, URL, USER, PASSWORD, "user_grocery_list");
-            lm.addItemToTable();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+public class TableManipulator {
+    String DATABASE_NAME;
+    String URL;
+    String USER;
+    String PASSWORD;
+    String TABLE_NAME;
+    //FIXME: Constructor
+    public TableManipulator(String DATABASE_NAME, String URL, String USER, String PASSWORD, String TABLE_NAME) {
+        this.DATABASE_NAME = DATABASE_NAME;
+        this.URL = URL;
+        this.USER = USER;
+        this.PASSWORD = PASSWORD;
+        this.TABLE_NAME = TABLE_NAME;
     }
-
-    public void utilFun() throws SQLException {
+    public void addItemToTable() throws SQLException {
         new Thread(() -> {
             StringBuilder records = new StringBuilder();
             try {
@@ -41,22 +27,20 @@ public class MainActivity extends AppCompatActivity {
                 Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
                 Statement statement = connection.createStatement();
 
-                ResultSet rs = statement.executeQuery("SELECT * FROM " + TABLE_NAME);
-                while (rs.next()) {
-                    String columnValue = rs.getString("name");
-                    Log.d("Database Result", columnValue);
-                }
+                statement.executeUpdate("INSERT INTO " + TABLE_NAME + " (name, category)" +
+                        //FIXME: Generalize insert values
+                        "VALUES (\"Apple\", \"Fruit\");" );
 
                 connection.close();
-
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            //Push working
         }).start();
     }
+}
 
-    public void makeListDB() throws SQLException {
+
+/*public void makeDB() throws SQLException {
         new Thread(() -> {
             StringBuilder records = new StringBuilder();
             try {
@@ -80,7 +64,4 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }).start();
-    }
-
-
-}
+    }*/
